f58f8c7d6e1f43f69b3115f326aea5e5aa5f6518
matthias1232 <matthias.1232@gmail.com>
2023-04-27 11:41:00 +0200

    HA State

---
+++ local/lib/check_mk/base/plugins/agent_based/snmp_sophosxg_sfospeerthastate.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import time
+
+from .agent_based_api.v1 import (
+    all_of,
+    startswith,
+    exists,
+    Metric,
+    register,
+    Result,
+    Service,
+    SNMPTree,
+    State,
+    get_value_store,
+    render,
+)
+
+
+
+register.snmp_section(
+    name="sophosxg_sfospeerthastate",
+    fetch=SNMPTree(
+        base=".1.3.6.1.4.1.2604.5.1.4",
+        oids = [
+        "5",
+        ],
+    ),
+    detect=all_of(
+        startswith(".1.3.6.1.2.1.1.2.0", ".1.3.6.1.4.1.2604.5"),
+        exists(".1.3.6.1.4.1.2604.5.1.1.*"),
+    ),
+)
+
+
+def discover_sophosxg_sfospeerthastate(section):
+    yield Service()
+
+
+def check_sophosxg_sfospeerthastate(section):
+    hastate = section[0][0]
+    hastatename = "Unknown"
+
+    if hastate == "0":
+        hastatename="Not Applicable"
+    if hastate == "1":
+        hastatename="Auxiliary"
+    if hastate == "2":
+        hastatename="Standalone"
+    if hastate == "3":
+        hastatename="Primary"
+    if hastate == "4":
+        hastatename="Faulty"
+    if hastate == "5":
+        hastatename="Ready"
+
+
+    if hastatename == "Auxiliary" or hastatename == "Primary":
+        state=State.OK
+    else:
+        state=State.CRIT
+
+    summarytext = "HA Peer Device State: " + str(hastatename) 
+    summarydetails = "For Support and Sales Please Contact K&P Computer! \n\n E-Mail: hds@kpc.de \n\n 24/7 Helpdesk-Support: \n International: +800 4479 3300 \n Germany: +49 6122 7071 330 \n Austria: +43 1 525 1833 \n\n Web Germany: https://www.kpc.de \n Web Austria: https://www.kpc.at \n Web International: https://www.kpc.de/en"
+
+    yield Result(
+        state=state,
+        summary=f"{summarytext} (Check Details if you need Help)",
+        details = summarydetails )
+
+register.check_plugin(
+    name="sophosxg_sfospeerthastate",
+    service_name="HA Peer Device State",
+    discovery_function=discover_sophosxg_sfospeerthastate,
+    check_function=check_sophosxg_sfospeerthastate,
+)
